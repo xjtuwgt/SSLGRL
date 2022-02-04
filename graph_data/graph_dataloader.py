43d8d4c5895fff5a7641361c9fa9c397f461b867
王广涛 <guangtao.wang@jd.com>
2022-02-04 14:55:08 -0800

    first commit

--- graph_data/graph_dataloader.py
+++ graph_data/graph_dataloader.py
@@ -1,7 +1,6 @@
 from dgl import DGLHeteroGraph
 import torch
 import dgl
-from numpy import random
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from graph_data.citation_graph_data import citation_k_hop_graph_reconstruction, citation_train_valid_test
@@ -9,68 +8,9 @@ from graph_data.ogb_graph_data import ogb_k_hop_graph_reconstruction, ogb_train_
 from core.graph_utils import sub_graph_neighbor_sample, cls_sub_graph_extractor, cls_anchor_sub_graph_augmentation
 
 
-class NodeSubGraphDataset(Dataset):
-    """
-    Graph representation learning without node labels
-    """
-
-    def __init__(self, graph: DGLHeteroGraph, nentity: int, nrelation: int, fanouts: list,
-                 special_entity2id: dict, special_relation2id: dict, bi_directed=True, edge_dir='in'):
-        assert len(fanouts) > 0
-        self.fanouts = fanouts  # list of int == number of hops for sampling
-        self.hop_num = len(fanouts)
-        self.g = graph
-        #####################
-        if len(special_entity2id) > 0:
-            self.len = graph.number_of_nodes() - len(special_entity2id)  # no sub-graph for special entities
-        else:
-            self.len = graph.number_of_nodes()
-        #####################
-        self.nentity, self.nrelation = nentity, nrelation
-        self.bi_directed = bi_directed
-        self.edge_dir = edge_dir  # "in", "out"
-        self.special_entity2id, self.special_relation2id = special_entity2id, special_relation2id
-
-    def __len__(self):
-        return self.len
-
-    def __getitem__(self, idx):
-        anchor_node_ids = torch.LongTensor([idx])
-        samp_hop_num = random.randint(2, self.hop_num + 1)
-        samp_fanouts = self.fanouts[:samp_hop_num]
-        cls_node_ids = torch.LongTensor([self.special_entity2id['cls']])
-        neighbors_dict, node_arw_label_dict, edge_dict = \
-            sub_graph_neighbor_sample(graph=self.g, anchor_node_ids=anchor_node_ids,
-                                      cls_node_ids=cls_node_ids, fanouts=samp_fanouts,
-                                      edge_dir=self.edge_dir, debug=False)
-        subgraph, parent2sub_dict = cls_sub_graph_extractor(graph=self.g, edge_dict=edge_dict,
-                                                            neighbors_dict=neighbors_dict,
-                                                            special_relation_dict=self.special_relation2id,
-                                                            node_arw_label_dict=node_arw_label_dict,
-                                                            bi_directed=self.bi_directed, debug=False)
-        sub_anchor_id = parent2sub_dict[idx]
-        return subgraph, sub_anchor_id
-
-    @staticmethod
-    def collate_fn(data):
-        assert len(data[0]) == 2
-        batch_graph_cls = torch.as_tensor([_[0].number_of_nodes() for _ in data], dtype=torch.long)
-        batch_graph_cls = torch.cumsum(batch_graph_cls, dim=0) - 1
-        batch_graphs = dgl.batch([_[0] for _ in data])
-        # ++++++++++++++++++++++++++++++++++++++++
-        batch_anchor_id = torch.zeros(len(data), dtype=torch.long)
-        for idx, _ in enumerate(data):
-            if idx == 0:
-                batch_anchor_id[idx] = _[1]
-            else:
-                batch_anchor_id[idx] = _[1] + batch_graph_cls[idx - 1].data.item() + 1
-        # ++++++++++++++++++++++++++++++++++++++++
-        return {'batch_graph': (batch_graphs, batch_graph_cls, batch_anchor_id)}
-
-
 class NodePredSubGraphDataset(Dataset):
     """
-    Graph representation learning with node labels
+    Node Prediction based on sub-graph
     """
 
     def __init__(self, graph: DGLHeteroGraph, nentity: int, nrelation: int, fanouts: list,
@@ -147,45 +87,6 @@ class NodePredSubGraphDataset(Dataset):
         return {'batch_graph': (batch_graphs, batch_graph_cls, batch_anchor_id), 'batch_label': batch_label}
 
 
-class node_prediction_data_helper(object):
-    def __init__(self, graph, fanouts, number_of_nodes: int, number_of_relations: int, num_class: int,
-                 special_entity_dict: dict, special_relation_dict: dict, train_batch_size: int,
-                 val_batch_size: int, graph_type: str, edge_dir: str = 'in', self_loop: bool = False,
-                 node_split_idx: dict = None):
-
-        self.graph = graph
-        self.fanouts = fanouts
-        self.number_of_nodes = number_of_nodes
-        self.number_of_relations = number_of_relations
-        self.special_entity_dict = special_entity_dict
-        self.special_relation_dict = special_relation_dict
-        self.train_batch_size = train_batch_size
-        self.val_batch_size = val_batch_size
-        self.num_class = num_class
-        self.graph_type = graph_type
-        self.node_split_idx = node_split_idx
-        self.self_loop = self_loop
-        self.edge_dir = edge_dir
-
-    def data_loader(self, data_type):
-        dataset = NodePredSubGraphDataset(graph=self.graph, nentity=self.number_of_nodes,
-                                          nrelation=self.number_of_relations,
-                                          special_entity2id=self.special_entity_dict,
-                                          special_relation2id=self.special_relation_dict,
-                                          data_type=data_type, graph_type=self.graph_type,
-                                          edge_dir=self.edge_dir, self_loop=self.self_loop,
-                                          fanouts=self.fanouts, node_split_idx=self.node_split_idx)
-        if data_type in {'train'}:
-            batch_size = self.train_batch_size
-            shuffle = True
-        else:
-            batch_size = self.val_batch_size
-            shuffle = False
-        data_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle, pin_memory=True,
-                                 collate_fn=NodePredSubGraphDataset.collate_fn)
-        return data_loader
-
-
 class NodeClassificationSubGraphDataHelper(object):
     def __init__(self, config):
         self.config = config
@@ -200,9 +101,10 @@ class NodeClassificationSubGraphDataHelper(object):
             self.node_split_idx = None
         elif self.graph_type == 'ogb':
             graph, node_split_idx, node_features, number_of_nodes, number_of_relations, \
-            special_node_dict, special_relation_dict, n_classes, n_feats = ogb_k_hop_graph_reconstruction(dataset=self.config.ogb_node_name,
-                                                                                   hop_num=self.config.sub_graph_hop_num,
-                                                                                   OON=self.config.oon_type)
+            special_node_dict, special_relation_dict, n_classes, n_feats = ogb_k_hop_graph_reconstruction(
+                dataset=self.config.ogb_node_name,
+                hop_num=self.config.sub_graph_hop_num,
+                OON=self.config.oon_type)
             self.node_split_idx = node_split_idx
         else:
             raise '{} is not supported'.format(self.graph_type)
