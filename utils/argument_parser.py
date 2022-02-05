a1e1c8e50357a6b7dffa6e67935bd62e2d733b17
王广涛 <guangtao.wang@jd.com>
2022-02-04 17:07:39 -0800

    first commit

--- utils/argument_parser.py
+++ utils/argument_parser.py
@@ -116,7 +116,7 @@ def default_parser():
     parser.add_argument('--alpha', type=float, default=0.1)
     parser.add_argument('--hidden_dim', type=int, default=256)
     parser.add_argument('--head_num', type=int, default=8)
-    parser.add_argument('--layers', type=int, default=2)
+    parser.add_argument('--layers', type=int, default=6)
     parser.add_argument('--negative_slope', type=float, default=0.2)
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     parser.add_argument('--eval_batch_size', type=int, default=32)
