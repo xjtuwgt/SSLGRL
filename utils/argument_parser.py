b9ecfde57f1dde7c0077b97fbd4df487b41c083e
王广涛 <guangtao.wang@jd.com>
2022-02-07 10:16:09 -0800

    first commit

--- utils/argument_parser.py
+++ utils/argument_parser.py
@@ -114,7 +114,7 @@ def default_parser():
     parser.add_argument('--ppr_diff', type=boolean_string, default='true')
     parser.add_argument('--stanford', type=boolean_string, default='false')
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    parser.add_argument('--gnn_hop_num', type=int, default=6)
+    parser.add_argument('--gnn_hop_num', type=int, default=10)
     parser.add_argument('--alpha', type=float, default=0.1)
     parser.add_argument('--hidden_dim', type=int, default=512)
     parser.add_argument('--head_num', type=int, default=8)
