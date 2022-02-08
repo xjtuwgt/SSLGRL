d994a4d171d62febf59236a3eb581741f67d427a
王广涛 <guangtao.wang@jd.com>
2022-02-07 21:39:06 -0800

    first commit

--- utils/experiment_utils.py
+++ utils/experiment_utils.py
@@ -38,7 +38,7 @@ def citation_hyper_parameter_space():
     edge_drop_ratio = {'name': 'edge_drop_ratio', 'type': 'choice', 'values': list(np.arange(0.05, 0.26, 0.025))}
     hop_num = {'name': 'hop_num', 'type': 'choice', 'values': [6, 8, 9, 10]}
     alpha = {'name': 'alpha', 'type': 'choice', 'values': list(np.arange(0.05, 0.21, 0.025))}
-    hidden_dim = {'name': 'hidden_dim', 'type': 'choice', 'values': [512]}
+    hidden_dim = {'name': 'hidden_dim', 'type': 'choice', 'values': [256]}
     layer_num = {'name': 'layer_num', 'type': 'choice', 'values': [3]}
     epoch = {'name': 'epoch', 'type': 'choice', 'values': [300]}
     # ++++++++++++++++++++++++++++++++++
