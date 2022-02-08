7479e093f5e495e8e6f3ae7d8df020919399d567
王广涛 <guangtao.wang@jd.com>
2022-02-07 21:30:51 -0800

    first commit

--- utils/experiment_utils.py
+++ utils/experiment_utils.py
@@ -1,10 +1,76 @@
 from graph_data.graph_dataloader import NodeClassificationSubGraphDataHelper
 import logging
+import numpy as np
 from tqdm import tqdm, trange
 from codes.gnn_predictor import NodeClassificationModel
 import torch
-from utils.basic_utils import IGNORE_IDX
+from utils.basic_utils import IGNORE_IDX, seed_everything
 from codes.gnn_encoder import GraphSimSiamEncoder
+
+
+def rand_search_parameter(space: dict):
+    para_type = space['type']
+    if para_type == 'fixed':
+        return space['value']
+    if para_type == 'choice':
+        candidates = space['values']
+        value = np.random.choice(candidates, 1).tolist()[0]
+        return value
+    if para_type == 'range':
+        log_scale = space.get('log_scale', False)
+        low, high = space['bounds']
+        if log_scale:
+            value = np.random.uniform(low=np.log(low), high=np.log(high), size=1)[0]
+            value = np.exp(value)
+        else:
+            value = np.random.uniform(low=low, high=high, size=1)[0]
+        return value
+    else:
+        raise ValueError('Training batch mode %s not supported' % para_type)
+
+
+def citation_hyper_parameter_space():
+    learning_rate = {'name': 'learning_rate', 'type': 'choice',
+                     'values': [1e-5, 2e-5, 5e-5]}  # 5e-5, 1e-4, 2e-4, 3e-4, 1e-3, 2e-3, 5e-3
+    weight_decay = {'name': 'weight_decay', 'type': 'choice', 'values': [1e-7, 5e-7]}
+    attn_drop_ratio = {'name': 'attn_drop_ratio', 'type': 'choice', 'values': list(np.arange(0.1, 0.56, 0.025))}
+    feat_drop_ratio = {'name': 'feat_drop_ratio', 'type': 'choice', 'values': list(np.arange(0.1, 0.56, 0.025))}
+    edge_drop_ratio = {'name': 'edge_drop_ratio', 'type': 'choice', 'values': list(np.arange(0.05, 0.26, 0.025))}
+    hop_num = {'name': 'hop_num', 'type': 'choice', 'values': [6, 8, 9, 10]}
+    alpha = {'name': 'alpha', 'type': 'choice', 'values': list(np.arange(0.05, 0.21, 0.025))}
+    hidden_dim = {'name': 'hidden_dim', 'type': 'choice', 'values': [64]}
+    layer_num = {'name': 'layer_num', 'type': 'choice', 'values': [2]}
+    epoch = {'name': 'epoch', 'type': 'choice', 'values': [300]}
+    # ++++++++++++++++++++++++++++++++++
+    search_space = [learning_rate, weight_decay, attn_drop_ratio, feat_drop_ratio, edge_drop_ratio,
+                    hidden_dim, hop_num, alpha, layer_num, epoch]
+    search_space = dict((x['name'], x) for x in search_space)
+    return search_space
+
+
+def single_task_trial(search_space: dict, rand_seed: int):
+    seed_everything(seed=rand_seed)
+    parameter_dict = {}
+    for key, value in search_space.items():
+        parameter_dict[key] = rand_search_parameter(value)
+    parameter_dict['seed'] = rand_seed
+    return parameter_dict
+
+
+def citation_random_search_hyper_tunner(args, search_space: dict, seed: int):
+    parameter_dict = single_task_trial(search_space=search_space, rand_seed=seed)
+    args.fine_tuned_learning_rate = parameter_dict['learning_rate']
+    args.feat_drop = parameter_dict['feat_drop_ratio']
+    args.attn_drop = parameter_dict['attn_drop_ratio']
+    args.edge_drop = parameter_dict['edge_drop_ratio']
+    args.layers = parameter_dict['layer_num']
+    args.gnn_hop_num = parameter_dict['hop_num']
+    args.alpha = parameter_dict['alpha']
+    args.hidden_dim = parameter_dict['hidden_dim']
+    args.fine_tuned_weight_decay = parameter_dict['weight_decay']
+    args.seed = parameter_dict['seed']
+    args.num_train_epochs = parameter_dict['epoch']
+    return args, parameter_dict
 
 
 def train_node_classification(args):
@@ -51,7 +117,7 @@ def train_node_classification(args):
     # **********************************************************************************
     logging.info('Starting fine tuning the model...')
     train_iterator = trange(start_epoch, start_epoch + int(args.num_train_epochs), desc="Epoch",
-                               disable=args.local_rank not in [-1, 0])
+                            disable=args.local_rank not in [-1, 0])
     for epoch_idx in train_iterator:
         epoch_iterator = tqdm(train_dataloader, desc="Iteration", disable=args.local_rank not in [-1, 0])
         for step, batch in enumerate(epoch_iterator):
@@ -103,7 +169,33 @@ def evaluate_node_classification_model(model, node_data_helper, args, data_type=
             preds = torch.argmax(logits, dim=-1)
             total_example = total_example + preds.shape[0]
             total_correct = total_correct + (preds == batch['batch_label']).sum().data.item()
-    eval_acc = total_correct/total_example
+    eval_acc = total_correct / total_example
     return eval_acc
 
 
+def hyper_parameter_tuning_rand_search(args):
+    num_of_experiments = args.exp_number
+    hyper_search_space = citation_hyper_parameter_space()
+    acc_list = []
+    search_best_test_acc = 0.0
+    search_best_val_acc = 0.0
+    search_best_settings = None
+    for _ in range(num_of_experiments):
+        args, hyper_setting_i = citation_random_search_hyper_tunner(args=args, search_space=hyper_search_space,
+                                                                    seed=args.seed + _)
+        best_val_acc, best_test_acc = train_node_classification(args=args)
+        acc_list.append((hyper_setting_i, best_val_acc, best_test_acc))
+        logging.info('*' * 50)
+        logging.info('{}\t{:.4f}\t{:.4f}'.format(hyper_setting_i, best_val_acc, best_test_acc))
+        logging.info('*' * 50)
+        if search_best_val_acc < best_val_acc:
+            search_best_val_acc = best_val_acc
+            search_best_test_acc = best_test_acc
+            search_best_settings = hyper_setting_i
+        logging.info('Current best testing acc = {:.4f} and best dev acc = {}'.format(search_best_test_acc,
+                                                                                     search_best_val_acc))
+        logging.info('*' * 30)
+    for _, setting_acc in enumerate(acc_list):
+        print(_, setting_acc)
+    print(search_best_test_acc)
+    print(search_best_settings)
