e12bf9e5fffb8e918383f54674b1bd92adb20f7f
王广涛 <guangtao.wang@jd.com>
2022-02-07 22:03:06 -0800

    first commit

--- utils/experiment_utils.py
+++ utils/experiment_utils.py
@@ -32,14 +32,14 @@ def rand_search_parameter(space: dict):
 def citation_hyper_parameter_space():
     learning_rate = {'name': 'learning_rate', 'type': 'choice',
                      'values': [1e-5, 2e-5, 5e-5]}  # 5e-5, 1e-4, 2e-4, 3e-4, 1e-3, 2e-3, 5e-3
-    weight_decay = {'name': 'weight_decay', 'type': 'choice', 'values': [1e-7, 5e-7]}
+    weight_decay = {'name': 'weight_decay', 'type': 'choice', 'values': [1e-8, 5e-8]}
     attn_drop_ratio = {'name': 'attn_drop_ratio', 'type': 'choice', 'values': list(np.arange(0.1, 0.56, 0.025))}
     feat_drop_ratio = {'name': 'feat_drop_ratio', 'type': 'choice', 'values': list(np.arange(0.1, 0.56, 0.025))}
     edge_drop_ratio = {'name': 'edge_drop_ratio', 'type': 'choice', 'values': list(np.arange(0.05, 0.26, 0.025))}
-    hop_num = {'name': 'hop_num', 'type': 'choice', 'values': [6, 8, 9, 10]}
+    hop_num = {'name': 'hop_num', 'type': 'choice', 'values': [6, 8]}
     alpha = {'name': 'alpha', 'type': 'choice', 'values': list(np.arange(0.05, 0.21, 0.025))}
-    hidden_dim = {'name': 'hidden_dim', 'type': 'choice', 'values': [256]}
-    layer_num = {'name': 'layer_num', 'type': 'choice', 'values': [3]}
+    hidden_dim = {'name': 'hidden_dim', 'type': 'choice', 'values': [512]}
+    layer_num = {'name': 'layer_num', 'type': 'choice', 'values': [6]}
     epoch = {'name': 'epoch', 'type': 'choice', 'values': [300]}
     # ++++++++++++++++++++++++++++++++++
     search_space = [learning_rate, weight_decay, attn_drop_ratio, feat_drop_ratio, edge_drop_ratio,
