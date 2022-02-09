6a606d3bc168cc01d25a1c4dfc854ce16e2a6310
王广涛 <guangtao.wang@jd.com>
2022-02-08 18:54:29 -0800

    first commit

--- graphexample.py
+++ graphexample.py
@@ -23,26 +23,43 @@ def ER(n, p):
     return g
 
 
-n = 4
-p = 0.65
-G = ER(n, p)
-print(type(G))
+# n = 4
+# p = 0.65
+# G = ER(n, p)
+# print(type(G))
+#
+# # graph = dgl.from_networkx(nx_graph=G)
+# graph = dgl.rand_graph(4, 8)
+# print(graph)
+# adj_matrix = graph.adj()
+# print(adj_matrix.to_dense())
+# A = adj_matrix.to_dense()
+# D = torch.diag(1.0/A.sum(dim=1))
+# A_n = torch.matmul(D, A)
+# print(A_n)
+# print(A_n.sum(dim=1))
+#
+# A2 = torch.matmul(A_n, A_n)
+# print(A2.sum(dim=1))
+# A3 = torch.matmul(A2, A_n)
+# A4 = torch.matmul(A3, A_n)
+# A5 = torch.matmul(A4, A_n)
+# A6 = torch.matmul(A5, A_n)
+# print(A6)
 
-# graph = dgl.from_networkx(nx_graph=G)
-graph = dgl.rand_graph(4, 8)
+src_nodes = [0, 0, 1, 2, 3, 3, 4]
+dst_nodes = [1, 2, 2, 3, 4, 5, 5]
+
+graph = dgl.graph((src_nodes + dst_nodes, dst_nodes +  src_nodes))
 print(graph)
-adj_matrix = graph.adj()
-print(adj_matrix.to_dense())
-A = adj_matrix.to_dense()
-D = torch.diag(1.0/A.sum(dim=1))
-A_n = torch.matmul(D, A)
-print(A_n)
-print(A_n.sum(dim=1))
+graph.edata['rid'] = torch.zeros(graph.number_of_edges())
 
-A2 = torch.matmul(A_n, A_n)
-print(A2.sum(dim=1))
-A3 = torch.matmul(A2, A_n)
-A4 = torch.matmul(A3, A_n)
-A5 = torch.matmul(A4, A_n)
-A6 = torch.matmul(A5, A_n)
-print(A6)
+from utils.graph_utils import sub_graph_neighbor_sample
+
+neighbors_dict, node_arw_label_dict, edge_dict = \
+    sub_graph_neighbor_sample(graph=graph, anchor_node_ids=torch.LongTensor([0]),
+                          cls_node_ids=torch.LongTensor([7]), fanouts=[-1])
+
+print(neighbors_dict)
+print(edge_dict)
+print(node_arw_label_dict)
