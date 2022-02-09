3daaf1f541a604e5741a5110656c2ba4e530e436
王广涛 <guangtao.wang@jd.com>
2022-02-08 20:50:47 -0800

    first commit

--- utils/experiment_utils.py
+++ utils/experiment_utils.py
@@ -33,8 +33,8 @@ def citation_hyper_parameter_space():
     learning_rate = {'name': 'learning_rate', 'type': 'choice',
                      'values': [1e-5, 2e-5]}  # 5e-5, 1e-4, 2e-4, 3e-4, 1e-3, 2e-3, 5e-3
     weight_decay = {'name': 'weight_decay', 'type': 'choice', 'values': [1e-8, 5e-8]}
-    attn_drop_ratio = {'name': 'attn_drop_ratio', 'type': 'choice', 'values': [0.2, 0.25, 0.3, 0.35]}
-    feat_drop_ratio = {'name': 'feat_drop_ratio', 'type': 'choice', 'values': [0.2, 0.25, 0.3, 0.35]}
+    attn_drop_ratio = {'name': 'attn_drop_ratio', 'type': 'choice', 'values': [0.35]}
+    feat_drop_ratio = {'name': 'feat_drop_ratio', 'type': 'choice', 'values': [0.2, 0.25]}
     edge_drop_ratio = {'name': 'edge_drop_ratio', 'type': 'choice', 'values': [0.1, 0.15, 0.2]}
     hop_num = {'name': 'hop_num', 'type': 'choice', 'values': [6, 8]}
     alpha = {'name': 'alpha', 'type': 'choice', 'values': [0.05, 0.1, 0.15]}
