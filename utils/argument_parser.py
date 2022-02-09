c11a560b14b37528867081974995c8705ab9b02f
王广涛 <guangtao.wang@jd.com>
2022-02-08 16:48:50 -0800

    first commit

--- utils/argument_parser.py
+++ utils/argument_parser.py
@@ -109,8 +109,8 @@ def default_parser():
     parser.add_argument('--siam_dim', type=int, default=2048)
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     parser.add_argument('--gnn_v2', type=boolean_string, default='false')
-    parser.add_argument('--feat_drop', type=float, default=0.25)
-    parser.add_argument('--attn_drop', type=float, default=0.15)
+    parser.add_argument('--feat_drop', type=float, default=0.35)
+    parser.add_argument('--attn_drop', type=float, default=0.25)
     parser.add_argument('--residual', type=boolean_string, default='true')
     parser.add_argument('--diff_head_tail', type=boolean_string, default='true')
     parser.add_argument('--ppr_diff', type=boolean_string, default='true')
@@ -118,7 +118,7 @@ def default_parser():
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     parser.add_argument('--gnn_hop_num', type=int, default=6)
     parser.add_argument('--alpha', type=float, default=0.1)
-    parser.add_argument('--hidden_dim', type=int, default=256)
+    parser.add_argument('--hidden_dim', type=int, default=512)
     parser.add_argument('--head_num', type=int, default=4)
     parser.add_argument('--layers', type=int, default=6)
     parser.add_argument('--negative_slope', type=float, default=0.2)
